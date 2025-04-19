462be3deb4d0f1c29660f6f1c629ba860eecf199
alexhool <alexhool97@gmail.com>
2025-04-19 19:59:37 -0400

    fix main code

--- src/main.cpp
+++ src/main.cpp
@@ -1,86 +1,29 @@
 #include <Arduino.h>
-#include <ESP32PCNTEncoder.h>
-#include <SparkFun_TB6612.h>
-#include <VL53L0X.h>
-
-#define ENCA 11
-#define ENCB 10
-#define BIN1 22
-#define BIN2 21
-#define PWMB 20
-#define STBY 23
-
-#define WAVE_DISTANCE 25
-
-uint8_t speed = 150;
-
-int64_t openPos = 10000;
-int64_t closePos = 0;
-bool isOpen = false;
-
-VL53L0X tof;
-ESP32PCNTEncoder encoder(ENCA, ENCB);
-Motor motor(BIN1, BIN2, PWMB, 1, STBY);
+#include "memory.h"
+#include "buttons.h"
+#include "motor.h"
+#include "tof.h"
+#include "states.h"
 
 void setup() {
+  rgbLedWrite(RGB_BUILTIN, 0, 0, 255);
   Serial.begin(115200);
-  while (!Serial) {}
-  Serial.println("\n--Setup--");
+  delay(1000);
 
-  Serial.print("Initializing ToF...");
-  Wire.begin(6, 7);
-  tof.setTimeout(100); // gives up after 100ms
-  while (!tof.init()) {
-    Serial.print(".");
-    delay(500);
-  }
-  tof.setMeasurementTimingBudget(20000); // 20ms measurement time (default: 33ms)
-  tof.startContinuous();
-  Serial.println("Done");
+  Serial.println("\n--- Setup ---");
 
-  Serial.print("Initializing Motor...");
-  motor.standby();
-  while (!encoder.begin()) {
-    Serial.print(".");
-    delay(500);
-  }
-  encoder.resetPosition();
-  Serial.println("Done");
+  // Initialize components
+  setupMemory();
+  setupButtons();
+  setupMotor();
+  setupTof();
+  setupStates();
 
-  Serial.println("\n--Program--");
+  Serial.printf("\n--- Program ---\n");
 }
 
 void loop() {
-  if (tof.readRangeContinuousMillimeters() < WAVE_DISTANCE) {
-    int64_t pos = encoder.getPosition();
-    if (isOpen) {
-      rgbLedWrite(RGB_BUILTIN, 255, 0, 0);
-      Serial.println("Closing blinds...");
-      motor.drive(-speed);
-      while (pos > closePos) {
-        pos = encoder.getPosition();
-      }
-      encoder.setPosition(closePos);
-      if (pos != closePos) {
-        Serial.print("Pos: ");
-        Serial.println(pos);
-      }
-    } else {
-      rgbLedWrite(RGB_BUILTIN, 0, 255, 0);
-      Serial.println("Opening blinds...");
-      motor.drive(speed);
-      while (pos < openPos) {
-        pos = encoder.getPosition();
-      }
-      encoder.setPosition(openPos);
-      if (pos != openPos) {
-        Serial.print("Pos: ");
-        Serial.println(pos);
-      }
-    }
-    rgbLedWrite(RGB_BUILTIN, 0, 0, 0);
-    motor.brake();
-    isOpen = !isOpen;
-  }
-  delay(10);
+  // Update system state machine
+  updateStateMachine();
+  delay(1);
 }
